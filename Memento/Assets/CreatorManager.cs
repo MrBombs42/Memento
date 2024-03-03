f4ac1e04a1178edcd9e08ec9b62cb8678dd6aafe
Stefano-Bombs <stefano.zanchetta@insanemedia.com.br>
2024-03-03 01:04:11 -0300

    more

--- Memento/Assets/CreatorManager.cs
+++ Memento/Assets/CreatorManager.cs
@@ -1,22 +1,40 @@
-﻿using System;
+﻿using Assets.Snapshots.Events;
+using Memento;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets
 {
-	public class CreatorManager : MonoBehaviour
+	public class CreatorManager : MementoBehavior
 	{
 
 		// aqui vai criar o objeto, caretaker vai salvar em q frame isso ocorreu, e no nome do modelo para poder recriar o objeto, posição e rotação
-
-		public event Action<long, string> OnInstanceCreated;
+		private CreationEventSnapshot _eventSnapshot;
 
 		private void Update()
 		{
 
 			if (Input.GetKeyDown(KeyCode.C))
 			{
-				OnInstanceCreated?.Invoke(1, "objecto");
+				_eventSnapshot.CreateList.Add(new CreationData()
+				{
+					ModelName = "Bla",
+					Position = new SerializablePosition(1, 2, 3)
+				});
 			}
+		}
+
+		public override ISnapshot GetSnapshot()
+		{
+			var copy = new CreationEventSnapshot();
+			copy.CreateList = _eventSnapshot.CreateList.ToList();
+
+			return copy;
+		}
+
+		public override void Restore(ISnapshot memento)
+		{
+
 		}
 	}
 }
--- Memento/Assets/Snapshots/Events/CreationEventSnapshot.cs
+++ Memento/Assets/Snapshots/Events/CreationEventSnapshot.cs
@@ -1,9 +1,16 @@
-﻿namespace Assets.Snapshots.Events
+﻿using System.Collections.Generic;
+
+namespace Assets.Snapshots.Events
 {
 	public class CreationEventSnapshot : IEvent
 	{
 		public SnapshotEventType EventType => SnapshotEventType.Created;
+		public List<CreationData> CreateList = new List<CreationData>();
 
+	}
+
+	public class CreationData
+	{
 		public SerializablePosition Position;
 		public SerializableQuaternion Rotation;
 		public string ModelName;
